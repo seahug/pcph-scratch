98d4381d1fa91af36920fb92cbf844f4759bdc98
Richard Cook <rcook@rcook.org>
2016-04-27 20:35:19 -0700

    Reader example

--- src/Main.hs
+++ src/Main.hs
@@ -4,30 +4,16 @@ import Prelude hiding (Functor, fmap)
 class Functor f where
     fmap :: (a -> b) -> f a -> f b
 
-instance Functor Tree where
-    fmap = mapT
+data Reader e a = Reader (e -> a)
 
-data I a = I a deriving Show
+instance Functor (Reader e) where
+    fmap g (Reader f) = Reader (g . f)
 
-instance Functor I where
-    fmap f (I x) = I (f x)
-
-mapMaybe :: (a -> b) -> Maybe a -> Maybe b
-mapMaybe _ Nothing = Nothing
-mapMaybe f (Just a) = Just (f a)
-
-data Tree a = Empty | Node (Tree a) a (Tree a) deriving Show
-
-sumT :: Tree Int -> Int
-sumT Empty = 0
-sumT (Node l x r) = sumT l + x + sumT r
-
-mapT :: (a -> b) -> Tree a -> Tree b
-mapT _ Empty = Empty
-mapT f (Node l x r) = Node (mapT f l) (f x) (mapT f r)
+get :: Reader e a -> e -> a
+get (Reader f) x = f x
 
 main :: IO ()
 main = do
-    let i = I 1234
-    print i
-    print $ fmap (*1000) i
+    let r = Reader (+100) :: Reader Int Int
+        r1 = fmap (*2) r
+    print $ get r1 123
